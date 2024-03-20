ab3754a775db1846361b9e9c886625e5c81b3efc
pxlmady <yoplufre@gmail.com>
2024-03-20 13:11:33 +0200

    some fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Description from "../Description/Description";
 import Options from "../Options/Options";
@@ -7,54 +7,52 @@ import Notification from "../Notification/Notification";
 
 function App() {
   const STORAGE_KEY = "feedbackData";
-  const initialFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+  const [guestOpinion, setguestOpinion] = useState(() => {
+    const initialData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    return initialData ?? { good: 0, neutral: 0, bad: 0 };
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setguestOpinion((prevOpinion) => ({
+      ...prevOpinion,
+      [feedbackType]: prevOpinion[feedbackType] + 1,
+    }));
   };
 
-  const [guestOpinion, setguestOpinion] = useState(initialFeedbackData);
-  const updateFeedback = (feedbackType) => {
-    setguestOpinion({
-      ...guestOpinion,
-      [feedbackType]: guestOpinion[feedbackType] + 1,
-    });
-  };
   const totalFeedback =
     guestOpinion.good + guestOpinion.bad + guestOpinion.neutral;
+
   const percentOfPositiveFeedback = Math.round(
     ((guestOpinion.good + guestOpinion.neutral) / totalFeedback) * 100
   );
+
   const handleResetButtonClick = () => {
-    setguestOpinion({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
+    setguestOpinion({ good: 0, neutral: 0, bad: 0 });
   };
+
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(guestOpinion));
-  });
+  }, [guestOpinion]);
+
   return (
-    <>
-      <div className="cafePlace">
-        <Description />
-        <Options
-          updateFeedback={updateFeedback}
-          totalFeedbackNumber={totalFeedback}
-          onResetButtonClick={handleResetButtonClick}
+    <div className="cafePlace">
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedbackNumber={totalFeedback}
+        onResetButtonClick={handleResetButtonClick}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          guestOpinion={guestOpinion}
+          total={totalFeedback}
+          positive={percentOfPositiveFeedback}
         />
-        {totalFeedback === 0 ? (
-          <Notification />
-        ) : (
-          <Feedback
-            guestOpinion={guestOpinion}
-            total={totalFeedback}
-            positive={percentOfPositiveFeedback}
-          />
-        )}
-      </div>
-    </>
+      )}
+    </div>
   );
 }
 
